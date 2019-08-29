0ac7aab3c6e04f45fb17bbaf2b8cbc1c0de53117
David Chia <taiwei.chia@fwd.com>
2019-08-29 15:49:06 +0800

    dynamiclink

--- src/main/java/com/fwd/affiliate/cms/linkshare/linkshare/IndexController.java
+++ src/main/java/com/fwd/affiliate/cms/linkshare/linkshare/IndexController.java
@@ -1,13 +1,30 @@
 package com.fwd.affiliate.cms.linkshare.linkshare;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-	return "Spring Boot iframe";
+	return "";
+    }
+    
+    @GetMapping("/link")
+    public String link(@RequestParam(name="link", required=true)String link, Model model) 
+    {
+	
+	
+	System.out.println("link: " + link);
+	model.addAttribute("link", link);
+	
+	String meta = "test";
+	
+	
+	return "index";
     }
 }
