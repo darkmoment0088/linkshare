161ba1f4ed0426ef67ba63543362681c48d13a4c
David Chia <taiwei.chia@fwd.com>
2019-08-30 14:13:17 +0800

    meta mimic

--- src/main/java/com/fwd/affiliate/cms/linkshare/linkshare/IndexController.java
+++ src/main/java/com/fwd/affiliate/cms/linkshare/linkshare/IndexController.java
@@ -1,10 +1,26 @@
 package com.fwd.affiliate.cms.linkshare.linkshare;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import org.xml.sax.SAXException;
 
 @Controller
 public class IndexController {
@@ -13,18 +29,19 @@ public class IndexController {
     public String index() {
 	return "";
     }
-    
+
     @GetMapping("/link")
-    public String link(@RequestParam(name="link", required=true)String link, Model model) 
-    {
+    public String link(@RequestParam(name = "link", required = true) String link, Model model)
+	    throws ParserConfigurationException, SAXException, IOException {
+	Document doc = Jsoup.connect(link).get();
 	
+	String metaList = doc.select("meta").outerHtml();
+
+	System.out.println("metaList"+metaList);
 	
-	System.out.println("link: " + link);
 	model.addAttribute("link", link);
-	
-	String meta = "test";
-	
-	
+	model.addAttribute("metaList", metaList);
+
 	return "index";
     }
 }
