75498b5d310da6d53d82a247d89e7167748bae93
David Chia <taiwei.chia@fwd.com>
2019-09-20 14:33:00 +0800

    added new model/Affiliate.java

---
+++ src/main/java/com/fwd/affiliate/cms/linkshare/model/Affiliate.java
@@ -0,0 +1,26 @@
+package com.fwd.affiliate.cms.linkshare.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Affiliate {
+    private long id;
+    private String userName;
+    private String userCode;
+    private String utmCode;
+    
+    private String ancestorAgentCode;
+    private String parentAffiliateId;
+    private String parentPath;
+    private String gender;
+    private String userType;
+}
